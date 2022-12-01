bcf5fc8de7fcb59ad15118573cbdd46fffb3e279
sang <sang93@qq.com>
2022-12-01 18:07:31 +0800

    完成基础数据获取显示

---
+++ src/BlogPreviewWebview.ts
@@ -0,0 +1,76 @@
+import * as vscode from "vscode";
+import * as path from "path";
+import * as fs from "fs";
+
+class BlogView{
+    context: vscode.ExtensionContext;
+    view: vscode.WebviewPanel;
+    isDisposed: boolean = false;
+
+    configureWebviewScripts(webviewScripts: string[]) {
+        //webviewScripts.push("libs/d3.js");
+        return webviewScripts;
+    }
+
+    constructor(context: vscode.ExtensionContext) {
+        this.context = context;
+
+        this.view = vscode.window.createWebviewPanel(
+            "blogPreview",
+            "Markdown Blog Preview",
+            vscode.ViewColumn.Two,
+            {
+                enableScripts: true,
+                localResourceRoots: [
+                    vscode.Uri.file(path.join(context.extensionPath, "html"))
+                ]
+            }
+        );
+        this.initialize();
+        this.isDisposed = false;
+    }
+
+    initialize() {
+        this.initializeWebviewHtml();
+        this.registerDisposables();
+    }
+    initializeWebviewHtml() {
+        let loadingScriptHtml: string[] = [];
+        this.configureWebviewScripts([]).forEach(path => {
+            var jsUri = vscode.Uri.file(this.getHtmlAssetPath(path)).with({
+                scheme: "vscode-resource"
+            });
+            loadingScriptHtml.push(
+                `<script src="${jsUri}"></script>`);
+        });
+
+        const html: string = fs.readFileSync(path.join(this.getHtmlAssetPath(
+            "webview.html"))).toString("utf-8");
+        this.view.webview.html = html.replace(
+            /<insert-vscode-resource\/>/g, loadingScriptHtml.join("\r\n"));
+    }
+    registerDisposables() {
+        this.view.onDidDispose(
+            () => {
+                this.isDisposed = true;
+            },
+            null,
+            this.context.subscriptions
+        );
+    }
+    getHtmlAssetPath(filename: string) {
+        return path.join(this.context.extensionPath, "html", filename);
+    }
+    updatePreview() {
+        const editingEditor = vscode.window.activeTextEditor;
+        if (editingEditor === undefined) {
+            vscode.window.showWarningMessage("活动编辑器无效");
+            return;
+        }
+        let data = editingEditor.document.getText();
+        this.view.webview.postMessage({
+            command: "renderMarkdown", data: data
+        });
+    }
+}
+export { BlogView };
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,30 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { BlogView } from "./BlogPreviewWebview";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "markdown-preview-blog" is now active!');
+	let blogPreview: BlogView;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('markdown-preview-blog.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Markdown Preview Blog!');
-	});
+	context.subscriptions.push(
+		vscode.commands.registerCommand('mdbp.showView', () => {
+			if ((blogPreview === undefined) || blogPreview.isDisposed) {
+				blogPreview = new BlogView(context);
+			}
+			blogPreview.updatePreview();
+		})
+	);
 
-	context.subscriptions.push(disposable);
+	 // 向 vscode 注册当前文件发生变化时的回调函数
+	 vscode.workspace.onDidChangeTextDocument((e: vscode.TextDocumentChangeEvent) => {
+        if (!!e && !!vscode.window.activeTextEditor && e.document.languageId === "markdown") {
+            blogPreview.updatePreview();
+        }
+    });
+	vscode.window.onDidChangeTextEditorSelection((e: vscode.TextEditorSelectionChangeEvent) => {
+        if (!!e && !!e.textEditor && (e.textEditor === vscode.window.activeTextEditor) && e.textEditor.document.languageId === "markdown") {
+            blogPreview.updatePreview();
+        }
+    });
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
